056a6e237cc89402d98398e760ec723d066de2f6
fgutier <fernando.gutierrez@morningstar.com>
2022-10-04 22:25:37 -0400

    A new test case was added ShareBar suite

---
+++ tests/business_insider/sharebar.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect, type Page } from '@playwright/test';
+import { PlanningStudioTourPage } from '../../src/page-objects/PlanningStudioTourPage';
+
+let planingStudioTourPage;
+
+test.beforeEach(async ({ page }) => {
+  planingStudioTourPage = new PlanningStudioTourPage(page);
+  await page.goto('https://www.businessinsider.com/ikea-nyc-store-planning-studio-tour-2019-4');
+});
+
+test.describe('Share bar suite', () => {
+  test('Assert that clicking the arrow expands the sharebar', async ({ page }) => {
+    await planingStudioTourPage.clickShareIcon();
+    expect(await planingStudioTourPage.isShareBarVisible()).toBeTruthy();
+  });
+
+  test('Assert that each button performs an action when clicked', async ({ page }) => {
+    await test.step('🧪 1. Verify that Insider logo is displayed.', async () => {
+      await planingStudioTourPage.clickSaveArticle();
+      expect.soft(await planingStudioTourPage.isInsiderLogoVisible()).toBeTruthy();
+
+      await planingStudioTourPage.clickCloseIcon();
+    });
+
+    await test.step('🧪 2. Assert that Facebook pop up title and its content is displayed.', async () => {
+      const facebookPopUp = await planingStudioTourPage.openFacebookPopup();
+
+      await expect.soft(facebookPopUp).toHaveTitle('Facebook');
+      await expect.soft(facebookPopUp.locator("#content")).toContainText("Log into your Facebook account to share.");
+
+      await facebookPopUp.close();
+    });
+
+    await test.step('🧪 3. Assert that Email pop up window is displayed.', async () => {
+      await planingStudioTourPage.clickEmailIcon();
+    });
+ });
+});
